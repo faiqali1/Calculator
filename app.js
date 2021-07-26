f6a200e330221dec8e58ec1af84a11119f52d115
faiqali1 <74228240+faiqali1@users.noreply.github.com>
2021-07-26 15:59:05 +0300

    added event listeners to add buttons

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const numbers = document.getElementsByClassName("numbers");
 const operands = document.getElementsByClassName("operand");
+
 const screen = document.getElementsByClassName("screen");
 const acButton = document.getElementById("ac-key");
 const delButton = document.getElementById("del");
 const decimal = document.getElementById(".")
 
-// decimal.addEventListener('click' ,function (){
-//     console.log("tapped");
-// })
+screen.addEventListener("click" , function(){})
+acButton.addEventListener("click" , function(){})
+delButton.addEventListener("click" , function(){})
+decimal.addEventListener("click" , function(){})
 
 
- for (let index = 0; index < numbers.length; index++) {
+for (let index = 0; index < numbers.length; index++) {
      numbers[index].addEventListener("click" ,function () {
          console.log("tapped!");
-         //TODO - add relevent function
+         //TODO - add relevant function
      } )
  }
 
@@ -22,7 +24,8 @@ for (let index = 0; index < operands.length; index++) {
     const element = operands[index];
     element.addEventListener('click', function (){
         console.log("touch");
-        //TODO - add relevent function
+        //TODO - add relevant function
     })
 }
+
 
